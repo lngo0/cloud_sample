0dd119b49b9d9f8b7021ad8b3ba61d06f13344df
lngo0 <longngo.gm@gmail.com>
2023-06-12 16:31:18 -0500

    Sample for kubernetes in action on GCP, create K8S engine, cloud SQL database instance
    Update folder name for terraform AWS example

---
+++ terraform-gcp/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> 4.37.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.gcp_project_id
+}
+
+#Google Kubernetes Engine (GKE)
+resource "google_container_cluster" "primary" {
+  name               = "kubernetes-cluster-1"
+  location           = var.gcp_region
+  initial_node_count = 1
+  enable_autopilot = true
+  ip_allocation_policy {
+  }
+}
+
+#Cloud SQL
+resource "google_sql_database_instance" "instance" {
+  name             = "mysql-database-instance-1"
+  region           = var.gcp_region
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection  = "true"
+}
+
+resource "google_sql_database" "database" {
+  name     = "my-database"
+  instance = google_sql_database_instance.instance.name
+}
